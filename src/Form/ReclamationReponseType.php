e7cec0921accabcb0bcf5c81ec976846d67f27c7
Chebbihoussemeddine <79277235+Chebbihoussemeddine@users.noreply.github.com>
2021-04-30 01:17:11 +0100

    Add files via upload

---
+++ src/Form/ReclamationReponseType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reclamation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReclamationReponseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+
+        ->add('reponse')
+
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Reclamation::class,
+        ]);
+    }
+}
