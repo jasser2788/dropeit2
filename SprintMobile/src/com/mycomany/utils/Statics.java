bfbc82dd1be67a73a657a6e9a338f9b9f4bb1eac
arfmed <79940084+arfmed@users.noreply.github.com>
2021-05-21 02:36:52 +0100

    Add files via upload

--- SprintMobile/src/com/mycomany/utils/Statics.java
+++ SprintMobile/src/com/mycomany/utils/Statics.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycomany.utils;
+package Utils;
 
 /**
  *
- * @author Lenovo
+ * @author louay_yahyaoui
  */
 public class Statics {
-    
-    
-    public static final String BASE_URL="http://127.0.0.1:8000/";
+      public static final String BASE_URL="http://localhost:8000";
 }
